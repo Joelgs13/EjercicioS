2e56fbd9dcd02c1a6693015656d8580a5f97b5ae
Joelgs13 <050213jgonzalez@gmail.com>
2024-10-30 22:50:56 +0100

    conexion, modelo, estilos vacios

---
+++ src/main/java/MODEL/AnimalModel.java
@@ -0,0 +1,125 @@
+package MODEL;
+
+import java.sql.Blob;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class AnimalModel {
+    private int id;
+    private String nombre;
+    private String especie;
+    private String raza;
+    private String sexo;
+    private int edad;
+    private int peso;
+    private String observaciones;
+    private LocalDate fechaPrimeraConsulta;
+    private Blob foto;
+
+    public AnimalModel(int id, String nombre, String especie, String raza, String sexo, int edad, int peso, String observaciones, LocalDate fechaPrimeraConsulta, Blob foto) {
+        this.id = id;
+        this.nombre = nombre;
+        this.especie = especie;
+        this.raza = raza;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.peso = peso;
+        this.observaciones = observaciones;
+        this.fechaPrimeraConsulta = fechaPrimeraConsulta;
+        this.foto = foto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    public LocalDate getFechaPrimeraConsulta() {
+        return fechaPrimeraConsulta;
+    }
+
+    public void setFechaPrimeraConsulta(LocalDate fechaPrimeraConsulta) {
+        this.fechaPrimeraConsulta = fechaPrimeraConsulta;
+    }
+
+    public Blob getFoto() {
+        return foto;
+    }
+
+    public void setFoto(Blob foto) {
+        this.foto = foto;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AnimalModel animal = (AnimalModel) o;
+        return id == animal.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.ejercicios {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.ejercicios to javafx.fxml;
