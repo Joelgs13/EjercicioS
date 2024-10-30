3ba88e3f6043c6d5d2a3fcf00fac813db290bfeb
Joelgs13 <050213jgonzalez@gmail.com>
2024-10-30 23:35:15 +0100

    pantalla principal dibujada

---
+++ src/main/java/com/example/ejercicios/ListaDeAnimalesController.java
@@ -0,0 +1,80 @@
+package com.example.ejercicios;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ListaDeAnimalesController {
+
+    @FXML
+    private MenuItem miAniadirAnimal;
+
+    @FXML
+    private MenuItem miBorrarAnimal;
+
+    @FXML
+    private MenuItem miEditarAnimal;
+
+    @FXML
+    private MenuItem miInformacionAnimal;
+
+    @FXML
+    private MenuItem miInformacionAnimal1;
+
+    @FXML
+    private TableView<?> tablaPriv;
+
+    @FXML
+    private TableColumn<?, ?> tcEdad;
+
+    @FXML
+    private TableColumn<?, ?> tcEspecie;
+
+    @FXML
+    private TableColumn<?, ?> tcFecha;
+
+    @FXML
+    private TableColumn<?, ?> tcId;
+
+    @FXML
+    private TableColumn<?, ?> tcNombre;
+
+    @FXML
+    private TableColumn<?, ?> tcObservaciones;
+
+    @FXML
+    private TableColumn<?, ?> tcPeso;
+
+    @FXML
+    private TableColumn<?, ?> tcRaza;
+
+    @FXML
+    private TableColumn<?, ?> tcSexo;
+
+    @FXML
+    private TextField tfNombre;
+
+    @FXML
+    void aniadirAnimal(ActionEvent event) {
+
+    }
+
+    @FXML
+    void borrarAnimal(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editarAnimal(ActionEvent event) {
+
+    }
+
+    @FXML
+    void informacionAnimal(ActionEvent event) {
+
+    }
+
+}
