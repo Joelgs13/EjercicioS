f08e7579d344fa4608eabfadbc36f9dddb8aa28a
Joelgs13 <050213jgonzalez@gmail.com>
2024-10-31 06:31:09 +0100

    pantalla para agregar o editar dibujada

---
+++ src/main/java/com/example/ejercicios/AniadirEditarAnimalController.java
@@ -0,0 +1,56 @@
+package com.example.ejercicios;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class AniadirEditarAnimalController {
+
+    @FXML
+    private DatePicker fechaPrimeraConsulta;
+
+    @FXML
+    private Label lbNumeroDeSocios;
+
+    @FXML
+    private TextField tfEdad;
+
+    @FXML
+    private TextField tfEspecie;
+
+    @FXML
+    private TextField tfNombre;
+
+    @FXML
+    private TextField tfNumeroDeSocios;
+
+    @FXML
+    private TextField tfObservaciones;
+
+    @FXML
+    private TextField tfPeso;
+
+    @FXML
+    private TextField tfRaza;
+
+    @FXML
+    private TextField tfSexo;
+
+    @FXML
+    void cancelar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cargarImagen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void guardar(ActionEvent event) {
+
+    }
+
+}
